245ad727812bd4db7ac32cbc6e3b3f0da33f9bc5
noornabi07 <noornabiprogram07@gmail.com>
2023-05-07 08:40:51 +0600

    get data from database server and show in display

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Users from './components/Users/Users.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>
   },
+  {
+    path: '/users',
+    element: <Users></Users>,
+    loader: () => fetch('http://localhost:5000/users')
+  }
 ]);
 
 
