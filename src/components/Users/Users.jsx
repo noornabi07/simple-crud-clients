55856bb7a7934a6c9d4e3851aca223ef45b8e2b2
noornabi07 <noornabiprogram07@gmail.com>
2023-05-07 09:24:22 +0600

    delete data user from database done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,11 +3,28 @@ import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
     const users = useLoaderData();
+    const handleDelete = id =>{
+        console.log("This is Delete id:", id)
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE',
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0){
+                alert('deleted succussfully');
+            }
+        })
+    }
     return (
         <div>
             <h2>User is: {users.length}</h2>
             {
-                users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                users.map(user => 
+                <p key={user._id}>{user.name} : {user.email} : {user._id}<button
+                    onClick={() => handleDelete(user._id)}
+                >X</button> </p>)
             }
         </div>
     );
